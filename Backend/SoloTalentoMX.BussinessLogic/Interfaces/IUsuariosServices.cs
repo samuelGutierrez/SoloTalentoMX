acbf61680dc655cc57faaf4e8949725bd828b67b
Samuel Gutierrez Muñoz <samuelguti007@gmail.com>
2023-07-21 01:39:42 -0500

    correcion estructura del proyecto

--- Backend/SoloTalentoMX.BussinessLogic/Interfaces/IUsuariosServices.cs
+++
@@ -1,9 +0,0 @@
-﻿using SoloTalentoMX.BussinessLogic.Dto;
-
-namespace SoloTalentoMX.BussinessLogic.Interfaces
-{
-    public interface IUsuariosServices
-    {
-        Task<bool> CrearUsuario(UsuariosCreateDto dto);
-    }
-}
