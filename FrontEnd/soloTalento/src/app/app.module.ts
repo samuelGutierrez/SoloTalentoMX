d6e538ec4e198fcfe0011c47157ecf8578f68dac
Jose <hjose0650@gmail.com>
2023-07-21 23:04:02 -0500

    new front

---
+++ FrontEnd/soloTalento/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    UserComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FrontEnd/soloTalento/src/enviroment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    APIURL: 'https://localhost:5001',
+    apiReferenceModelUrl: 'http://localhost:5001'
+};
